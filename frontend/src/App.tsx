7e735d22aaf092d294e42bbc1e5366c706165378
vin-vu <vince.hvu@gmail.com>
2025-01-04 21:10:23 -0800

    Added prettierrc file and added task input form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-
+  const [value, setValue] = useState('');
   return (
     <>
-      <div className='main-container'>
+      <div className="main-container">
         <span>EchoTask</span>
-        
+        <div className="input-container">
+          <form>
+            <input
+              type="text"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+            <button type="submit">Add Task</button>
+          </form>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
