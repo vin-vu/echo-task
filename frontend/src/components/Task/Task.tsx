a631e9ccb4f8e4985e29fb89becb0e4cd82f321e
vin-vu <vince.hvu@gmail.com>
2025-01-26 18:59:50 -0800

    Fixed conditionally rendering description or input box when editing task

--- frontend/src/components/Task/Task.tsx
+++ frontend/src/components/Task/Task.tsx
@@ -1,6 +1,7 @@
 import { FaTrash } from 'react-icons/fa';
 import { TiEdit } from 'react-icons/ti';
 import './Task.css';
+import { useState } from 'react';
 
 type TaskProps = {
   id: string;
@@ -9,14 +10,27 @@ type TaskProps = {
   editTask: (id: string, newDescription: string) => void;
 };
 
-export default function Task({ id, description, deleteTask, editTask }: TaskProps) {
+export default function Task({
+  id,
+  description,
+  deleteTask,
+  editTask,
+}: TaskProps) {
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+
+  const handleEdit = () => {
+    setIsEdit(true);
+  };
+
+  const displayDescription = () =>
+    !isEdit ? <p>{description}</p> : <input></input>;
 
   return (
     <div className="task-item">
-      <p>{description}</p>
+      {displayDescription()}
       <div className="button-container">
         <FaTrash onClick={() => deleteTask(id)} />
-        <TiEdit />
+        <TiEdit onClick={() => handleEdit()} />
       </div>
     </div>
   );
