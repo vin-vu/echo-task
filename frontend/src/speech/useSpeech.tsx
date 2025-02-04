d12255feb4c3805fc3bc94cc4a89489d3ad6a2cb
vin-vu <vince.hvu@gmail.com>
2025-02-03 23:23:15 -0800

    Created useSpeech hook

--- frontend/src/speech/useSpeech.tsx
+++ frontend/src/speech/useSpeech.tsx
@@ -1,5 +1,29 @@
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
-
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from 'react-speech-recognition';
 
 export default function useSpeech() {
+  const {
+    transcript,
+    listening,
+    browserSupportsSpeechRecognition,
+    browserSupportsContinuousListening,
+    isMicrophoneAvailable,
+  } = useSpeechRecognition();
+
+  const startListening = (): void => {
+    if (browserSupportsContinuousListening) {
+      SpeechRecognition.startListening({ continuous: true });
+    } else {
+      SpeechRecognition.startListening();
+    }
+  };
+
+  return {
+    transcript,
+    listening,
+    browserSupportsSpeechRecognition,
+    startListening,
+    isMicrophoneAvailable,
+  };
 }
