ec3e49d0491e252566c30c75577d830b5e95fa49
vin-vu <vince.hvu@gmail.com>
2025-02-03 21:27:00 -0800

    Created animated ring around microphone icon

--- frontend/src/components/Microphone/Microphone.tsx
+++ frontend/src/components/Microphone/Microphone.tsx
@@ -1,9 +1,11 @@
 import { FaMicrophone } from 'react-icons/fa';
+import './Microphone.css';
 
 export default function Microphone() {
   return (
-    <div className="microphone-container">
-      <FaMicrophone size={75}/>
+    <div className="mic-container">
+      <div className="mic-circle"></div>
+      <FaMicrophone className="mic-icon" size={75} />
     </div>
   );
 }
