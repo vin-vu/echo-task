0be83db9d645abf3d71ebc68eb8877b061290c57
vin-vu <vince.hvu@gmail.com>
2025-02-05 12:55:41 -0800

    Updated detect intent endpoint with request body

--- backend/src/main/java/com/vince/echotask/controller/EchoTaskController.java
+++ backend/src/main/java/com/vince/echotask/controller/EchoTaskController.java
@@ -1,15 +1,18 @@
 package com.vince.echotask.controller;
 
+import com.vince.echotask.pojo.IntentRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 public class EchoTaskController {
 
-    @GetMapping("/detect-intent")
-    String detectIntent() {
-        return "Temp response containing intent";
-    }
+  @GetMapping("/detect-intent")
+  String detectIntent(@RequestBody IntentRequest request) {
+    log.info("Intent request: {}", request);
+    return "Temp response containing intent";
+  }
 }
---
+++ backend/src/main/java/com/vince/echotask/pojo/IntentRequest.java
@@ -0,0 +1,10 @@
+package com.vince.echotask.pojo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class IntentRequest {
+  private String transcription;
+}
