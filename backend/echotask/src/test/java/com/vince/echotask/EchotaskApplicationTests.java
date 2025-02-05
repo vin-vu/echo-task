5f33cbafbf5ccba33125cbe460c3fb9d1c0f6264
vin-vu <vince.hvu@gmail.com>
2025-02-05 09:49:41 -0800

    Added backend Sprin Boot boilerplate

---
+++ backend/echotask/src/test/java/com/vince/echotask/EchotaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vince.echotask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchotaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
