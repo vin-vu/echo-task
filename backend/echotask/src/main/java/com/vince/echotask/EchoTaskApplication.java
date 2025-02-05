22a9d9755484a328b0e362a57ff482e26275cb18
vin-vu <vince.hvu@gmail.com>
2025-02-05 10:39:47 -0800

    Created detect intent endpoint

---
+++ backend/echotask/src/main/java/com/vince/echotask/EchoTaskApplication.java
@@ -0,0 +1,1 @@
+package com.vince.echotask;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;@SpringBootApplicationpublic class EchoTaskApplication {	public static void main(String[] args) {		SpringApplication.run(EchoTaskApplication.class, args);	}}
---
+++ backend/echotask/src/main/java/com/vince/echotask/controller/EchoTaskController.java
@@ -0,0 +1,15 @@
+package com.vince.echotask.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class EchoTaskController {
+
+    @GetMapping("/detect-intent")
+    String detectIntent() {
+        return "Temp response containing intent";
+    }
+}
