8a6d61a0fe3935217f3d3d68eef9473273592208
vin-vu <vince.hvu@gmail.com>
2025-02-05 19:46:46 -0800

    Updated detect intent endpoint to return response entity

--- backend/src/main/java/com/vince/echotask/controller/EchoTaskController.java
+++ backend/src/main/java/com/vince/echotask/controller/EchoTaskController.java
@@ -1,7 +1,11 @@
 package com.vince.echotask.controller;
 
 import com.vince.echotask.pojo.IntentRequest;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class EchoTaskController {
 
   @PostMapping("/detect-intent")
-  String detectIntent(@RequestBody IntentRequest request) {
-    log.info("Intent request: {}", request);
-    return "Temp response containing intent";
+  ResponseEntity<Map<String, String>> detectIntent(@RequestBody IntentRequest request) {
+    log.info(request.toString());
+
+    Map<String, String> response = new HashMap<>();
+    response.put("intent", "Temp response containing intent");
+    return new ResponseEntity<>(response, HttpStatus.OK);
   }
 }
