146d3236f4814104c75bfec5db35019c4d7c9f18
vin-vu <vince.hvu@gmail.com>
2025-02-13 12:47:09 -0800

    Tokenizer model working

--- backend/src/main/java/com/vince/echotask/nlp/Tokenizer.java
+++ backend/src/main/java/com/vince/echotask/nlp/Tokenizer.java
@@ -3,6 +3,8 @@ package com.vince.echotask.nlp;
 import jakarta.annotation.PostConstruct;
 import java.io.*;
 import lombok.extern.slf4j.Slf4j;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
@@ -24,6 +26,23 @@ public class Tokenizer {
 
     } catch (IOException e) {
       throw new IOException(e);
+    }
+  }
+
+  @PostConstruct
+  public void tokenizeSpeech() throws IOException {
+    ClassPathResource modelResource =
+        new ClassPathResource("nlp/opennlp-en-ud-ewt-tokens-1.2-2.5.0.bin");
+    InputStream modelInput = modelResource.getInputStream();
+
+    TokenizerModel tokenizerModel = new TokenizerModel(modelInput);
+    TokenizerME tokenizer = new TokenizerME(tokenizerModel);
+
+    String text = "Sample text to be tokenized.";
+    String[] tokens = tokenizer.tokenize(text);
+
+    for (String token : tokens) {
+      log.info("token: {}", token);
     }
   }
 }
