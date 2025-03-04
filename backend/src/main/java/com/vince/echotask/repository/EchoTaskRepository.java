29ec1212306d3c0f908b097968bbdbe687a34c74
vin-vu <vince.hvu@gmail.com>
2025-03-03 17:15:47 -0800

    Created repository to interact with postgres

---
+++ backend/src/main/java/com/vince/echotask/repository/EchoTaskRepository.java
@@ -0,0 +1,11 @@
+package com.vince.echotask.repository;
+
+import com.vince.echotask.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EchoTaskRepository extends JpaRepository<Task, UUID> {
+}
