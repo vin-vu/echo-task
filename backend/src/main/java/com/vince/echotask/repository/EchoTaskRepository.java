9d666a242927554b8216d2d75f326c8f41217f94
vin-vu <vince.hvu@gmail.com>
2025-03-04 12:03:32 -0800

    Created tsvector delete query

--- backend/src/main/java/com/vince/echotask/repository/EchoTaskRepository.java
+++ backend/src/main/java/com/vince/echotask/repository/EchoTaskRepository.java
@@ -9,7 +9,12 @@ import java.util.UUID;
 @Repository
 public interface EchoTaskRepository extends JpaRepository<Task, UUID> {
 
-    default void deleteBestMatchingTask(String description) {
 
+    default void deleteBestMatchingTask(String input) {
+        String query = "DELETE FROM tasks WHERE id = ("
+                + "SELECT id FROM tasks "
+                + "WHERE to_tsvector('english', description) @@ plainto_tsquery(:input) "
+                + "ORDER BY ts_rank(to_tsvector('english', description), plainto_tsquery(:input)) DESC "
+                + "limit 1) RETURNING *";
     }
 }
