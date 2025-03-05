abbe28c64fbfd10c1376b0d62d92b18f29e1ce79
vin-vu <vince.hvu@gmail.com>
2025-03-04 18:48:30 -0800

    Created create task client api

---
+++ frontend/src/api/Api.tsx
@@ -0,0 +1,24 @@
+import { TaskData } from '../App';
+
+type CreateTaskResponse = {
+  id: string;
+};
+
+export const createTask = async (
+  task: TaskData
+): Promise<CreateTaskResponse | undefined> => {
+  const createTaskAPI = 'http://localhost:8080/create-task';
+  try {
+    const response = await fetch(createTaskAPI, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ task }),
+    });
+    const json: CreateTaskResponse = await response.json();
+    return json;
+  } catch (e) {
+    console.error(e);
+  }
+};
