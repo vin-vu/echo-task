9bda208e44b53b245614fd602730692cb27cc00f
vin-vu <vince.hvu@gmail.com>
2025-03-05 16:51:07 -0800

    Created delete task client

--- frontend/src/api/Api.tsx
+++ frontend/src/api/Api.tsx
@@ -23,3 +23,22 @@ export const createTask = async (
     console.error(e);
   }
 };
+
+export const deleteTask = async (
+  id: string
+): Promise<TaskData | undefined> => {
+  const createTaskAPI = 'http://localhost:8080/delete-task';
+  try {
+    const response = await fetch(createTaskAPI, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id }),
+    });
+    const json: TaskData = await response.json();
+    return json;
+  } catch (e) {
+    console.error(e);
+  }
+};
