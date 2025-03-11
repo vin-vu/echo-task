aab9371dd10f3e36f456b6a22374ed7324563475
vin-vu <vince.hvu@gmail.com>
2025-03-11 13:56:16 -0700

    Abstracted noun traversal strategy

---
+++ backend/src/main/java/com/vince/echotask/nlp/depparser/strategy/NounTraversalStrategy.java
@@ -0,0 +1,39 @@
+package com.vince.echotask.nlp.depparser.strategy;
+
+import edu.stanford.nlp.ling.IndexedWord;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphEdge;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+public class NounTraversalStrategy implements TraversalStrategy {
+
+    @Override
+    public void travserse(SemanticGraph dependencyParse, IndexedWord currentNode, List<IndexedWord> taskDescriptionWords) {
+        if (taskDescriptionWords.isEmpty()) taskDescriptionWords.add(currentNode);
+
+        List<IndexedWord> childrenNodes = dependencyParse.getChildList(currentNode);
+        log.info("parent - children: {}, {}", currentNode, childrenNodes);
+
+        for (IndexedWord childNode : childrenNodes) {
+            SemanticGraphEdge edge = dependencyParse.getEdge(currentNode, childNode);
+            String relation = edge.getRelation().toString();
+            log.info("2: edge - relation: {}, {}", edge, relation);
+
+            String pos = childNode.tag();
+
+            // skip intent verb
+            if (Objects.equals(pos, "VB") && Objects.equals(relation, "csubj")) {
+                continue;
+                // handle NN root with JJ intent amod see ex. utterance9
+            } else if (dependencyParse.getFirstRoot() == currentNode && Objects.equals(pos, "JJ") && Objects.equals(relation, "amod")) {
+                continue;
+            }
+            taskDescriptionWords.add(childNode);
+            travserse(dependencyParse, childNode, taskDescriptionWords);
+        }
+    }
+}
