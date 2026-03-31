8b4d58583a9d67c6cc9085a4e03b9c9925336c06
Vince <vince.hvu@gmail.com>
2026-03-30 18:44:47 -0700

    modularized tokenizer and implemented task finder

--- backend/src/main/java/com/vince/echotask/nlp/task/NERTestRunner.java
+++ backend/src/main/java/com/vince/echotask/nlp/task/NERTestRunner.java
@@ -1,11 +1,9 @@
 package com.vince.echotask.nlp.task;
 
 import lombok.extern.slf4j.Slf4j;
-import opennlp.tools.util.Span;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -27,28 +25,28 @@ public class NERTestRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        for (String input : testInputs) {
-            String[] tokens = input.split(" ");
-            Span[] spans = taskFinderService.extractTask(tokens);
-
-            log.info("INPUT: {}", input);
-
-            if (spans.length == 0) {
-                log.info("  → no entity found");
-                continue;
-            }
-
-            for (Span s : spans) {
-                String extracted = String.join(" ",
-                        Arrays.copyOfRange(tokens, s.getStart(), s.getEnd()));
-
-                log.info("  → span [{}..{}) type={} text='{}'",
-                        s.getStart(),
-                        s.getEnd(),
-                        s.getType(),
-                        extracted
-                );
-            }
-        }
+//        for (String input : testInputs) {
+//            String[] tokens = input.split(" ");
+//            Span[] spans = taskFinderService.extractTask(tokens);
+//
+//            log.info("INPUT: {}", input);
+//
+//            if (spans.length == 0) {
+//                log.info("  → no entity found");
+//                continue;
+//            }
+//
+//            for (Span s : spans) {
+//                String extracted = String.join(" ",
+//                        Arrays.copyOfRange(tokens, s.getStart(), s.getEnd()));
+//
+//                log.info("  → span [{}..{}) type={} text='{}'",
+//                        s.getStart(),
+//                        s.getEnd(),
+//                        s.getType(),
+//                        extracted
+//                );
+//            }
+//        }
     }
 }
